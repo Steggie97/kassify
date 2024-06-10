68bb4b6f7ced21484af2cf718d7aec8ca57f3431
Steggie97 <118690787+Steggie97@users.noreply.github.com>
2024-06-10 19:54:40 +0200

    Update Transaction.kt

--- app/src/main/java/com/ls/kassify/data/Transaction.kt
+++ app/src/main/java/com/ls/kassify/data/Transaction.kt
@@ -1,20 +1,13 @@
 package com.ls.kassify.data
 
 data class Transaction(
-    val transId: Int,
-    val date: String,
-    val amount: Double,
-    val category: String,
-    val receiptNo: String,
-    val text: String,
-    val transId: Int = 0,
-    val date: String = "01.01.2024",
-    val amount: Double = 0.00,
-    val category: String = "",
-    val receiptNo: String = "",
-    val text: String = "",
+    var transId: Int = 0,
+    var date: String = "01.01.2024",
+    var amount: Double = 0.00,
+    var category: String = "",
+    var receiptNo: String = "",
+    var text: String = "",
 )
-
 /*
 val transactions: List<Transaction> = listOf(
     Transaction(transId = 0, date = "02.05.2024", amount = -30.50, category = "laufende KFZ-Kosten", receiptNo = "Rg-Nr.12342", text = "Aral - tanken"),
@@ -22,6 +15,6 @@ val transactions: List<Transaction> = listOf(
     Transaction(transId = 2, date = "10.05.2024", amount = -10.78, category = "Büromaterial", receiptNo = "2", text = "Michelbrink - Schreibwaren"),
     Transaction(transId = 3, date = "21.05.2024", amount = 65.00, category = "Erlöse 19%", receiptNo = "RE2024/2", text = "Rechnung RE2024/2"),
     Transaction(transId = 4, date = "22.05.2024", amount = -111500.00, category = "Geldtransit", receiptNo = "", text = "Bankeinzahlung")
-))
+)
 
  */
