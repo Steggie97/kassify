616bb1ab23d1d6c6d8ab10df04ac70af8ccdf954
Maria Vatset <vatset@MacBook-Pro-Maria.local>
2024-06-10 08:52:18 +0200

    image added

--- app/src/main/java/com/ls/kassify/ui/screens/LogInScreen.kt
+++ app/src/main/java/com/ls/kassify/ui/screens/LogInScreen.kt
@@ -1,8 +1,11 @@
 package com.ls.kassify.ui.screens
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.foundation.layout.statusBarsPadding
@@ -20,12 +23,15 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.ls.kassify.R
 import com.ls.kassify.ui.CredentialFields
+
+
 
 @Composable
 fun LogInScreen(
@@ -47,8 +53,14 @@ fun LogInScreen(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-        //TODO: Logo über LogIn-Form einfuegen
-
+        val image = painterResource(R.drawable.file)
+        Image(
+            painter = image,
+            contentDescription = null,
+            modifier = Modifier
+                .fillMaxWidth(0.6f)
+                .padding(bottom = 16.dp)
+        )
         CredentialFields(
             email = email,
             showPassword = showPassword,
