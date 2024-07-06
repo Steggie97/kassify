e8917227d2e49d7a0d52de24a595d2237209c72a
Steggie97 <lukas.steggers@gmail.com>
2024-07-06 14:43:58 +0200

    Moving PieChart in LazyList

--- app/src/main/java/com/ls/kassify/ui/screens/TransactionListScreen.kt
+++ app/src/main/java/com/ls/kassify/ui/screens/TransactionListScreen.kt
@@ -66,80 +66,6 @@ fun TransactionListScreen(
         modifier = modifier
             .fillMaxSize()
     ) {
-        Column(
-            modifier = Modifier
-                .padding(
-                    top = 64.dp,
-                    bottom = 16.dp,
-                    start = 16.dp,
-                    end = 16.dp
-                )
-                .animateContentSize(
-                    animationSpec = spring(
-                        dampingRatio = Spring.DampingRatioNoBouncy,
-                        stiffness = Spring.StiffnessLow
-                    )
-                )
-        ) {
-            Box(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(top = 4.dp, bottom = 8.dp),
-                contentAlignment = Alignment.Center
-            ) {
-                Row() {
-                    Text(
-                        text =
-                        if (expanded)
-                            stringResource(R.string.pie_chart_hide_text)
-                        else
-                            stringResource(R.string.pie_chart_show_text),
-                        modifier = Modifier.clickable { expanded = !expanded },
-                        style = MaterialTheme.typography.titleMedium
-                    )
-                    FloatingActionButton(
-                        onClick = { expanded = !expanded },
-                        containerColor = MaterialTheme.colorScheme.background,
-                        elevation = FloatingActionButtonDefaults.elevation(defaultElevation = 0.dp),
-                        modifier = Modifier
-                            .size(30.dp)
-                            .padding(start = 4.dp),
-                    ) {
-                        if (expanded) {
-                            Image(
-                                painter = painterResource(R.drawable.keyboard_double_arrow_up_24px),
-                                contentDescription = null,
-                                colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.primary),
-                                modifier = Modifier.size(30.dp)
-                            )
-                        } else {
-                            Image(
-                                painter = painterResource(R.drawable.keyboard_double_arrow_down_24px),
-                                contentDescription = null,
-                                colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.primary),
-                                modifier = Modifier.size(30.dp)
-                            )
-                        }
-
-                    }
-                }
-            }
-            if (expanded) {
-                    Text(
-                        text = stringResource(R.string.piechart_title_text),
-                        style = MaterialTheme.typography.bodySmall,
-                        modifier = Modifier.fillMaxWidth(),
-                        textAlign = TextAlign.Center
-                    )
-                    PieChartView(
-                        dataEntries = dataEntries,
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .height(320.dp)
-                            .padding(bottom = 8.dp)
-                    )
-            }
-
             LazyColumn(
                 verticalArrangement = Arrangement.spacedBy(16.dp),
                 // the lastTransaction is shown on the top of the list
@@ -162,6 +88,77 @@ fun TransactionListScreen(
                         modifier = Modifier.padding(bottom = 32.dp)
                     )
                 }
+                item { Column(
+                    modifier = Modifier
+                        .padding(
+                            top = 64.dp,
+                            start = 16.dp,
+                            end = 16.dp
+                        )
+                        .animateContentSize(
+                            animationSpec = spring(
+                                dampingRatio = Spring.DampingRatioNoBouncy,
+                                stiffness = Spring.StiffnessLow
+                            )
+                        )
+                ) {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(top = 4.dp, bottom = 8.dp),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Row() {
+                            Text(
+                                text =
+                                if (expanded)
+                                    stringResource(R.string.pie_chart_hide_text)
+                                else
+                                    stringResource(R.string.pie_chart_show_text),
+                                modifier = Modifier.clickable { expanded = !expanded },
+                                style = MaterialTheme.typography.titleMedium
+                            )
+                            FloatingActionButton(
+                                onClick = { expanded = !expanded },
+                                containerColor = MaterialTheme.colorScheme.background,
+                                elevation = FloatingActionButtonDefaults.elevation(defaultElevation = 0.dp),
+                                modifier = Modifier
+                                    .size(30.dp)
+                                    .padding(start = 4.dp),
+                            ) {
+                                if (expanded) {
+                                    Image(
+                                        painter = painterResource(R.drawable.keyboard_double_arrow_up_24px),
+                                        contentDescription = null,
+                                        colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.primary),
+                                        modifier = Modifier.size(30.dp)
+                                    )
+                                } else {
+                                    Image(
+                                        painter = painterResource(R.drawable.keyboard_double_arrow_down_24px),
+                                        contentDescription = null,
+                                        colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.primary),
+                                        modifier = Modifier.size(30.dp)
+                                    )
+                                }
+                            }
+                        }
+                    }
+                    if (expanded) {
+                        Text(
+                            text = stringResource(R.string.piechart_title_text),
+                            style = MaterialTheme.typography.bodySmall,
+                            modifier = Modifier.fillMaxWidth(),
+                            textAlign = TextAlign.Center
+                        )
+                        PieChartView(
+                            dataEntries = dataEntries,
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .height(320.dp)
+                        )
+                    }
+                }
             }
         }
         // Add button for creating new transactions
@@ -177,9 +174,7 @@ fun TransactionListScreen(
                 painter = painterResource(R.drawable.add_icon), contentDescription = null,
                 colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.background)
             )
-
         }
-
         // shows the current cashBalance in list
         CashBalanceBox(cashBalance = cashBalance)
     }
